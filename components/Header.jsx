4e619b8c59d87d4ff911ebaf58b70905da5a7a7e
Muhamed-Kanapiya <muhamed08@mail.ru>
2024-04-19 02:41:50 +0500

    added logo and nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header>
       <nav className="wrapper">
+        <img src="../src/favicon.png" width="50px" />
         <span className="logo__title">Frontend Tools</span>
       </nav>
     </header>

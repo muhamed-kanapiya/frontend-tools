5c6d63aa1f00fd090fe503afbbbfc4f0e27aef5a
Muhamed-Kanapiya <muhamed08@mail.ru>
2024-04-19 02:37:44 +0500

    some design

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="wrapper">
+        <span className="logo__title">Frontend Tools</span>
+      </nav>
+    </header>
+  );
+}

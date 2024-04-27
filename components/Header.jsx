6df2db2fa6940f3eb144945ddccf52a8d8cf03c4
Muhamed-Kanapiya <muhamed08@mail.ru>
2024-04-28 01:17:17 +0500

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <nav className="wrapper">
-        <img src="../src/favicon.png" width="50px" />
+        <img src="../public/favicon.png" width="50px" />
         <span className="logo__title">Frontend Tools</span>
       </nav>
     </header>
